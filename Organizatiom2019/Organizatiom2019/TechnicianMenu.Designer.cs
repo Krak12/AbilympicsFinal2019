d5a7de5235ed8fc96eab0dd1a385238162629898
Krak12 <mamukin08@gmail.com>
2019-09-23 21:06:50 +0500

    Save

--- Organizatiom2019/Organizatiom2019/TechnicianMenu.Designer.cs
+++ Organizatiom2019/Organizatiom2019/TechnicianMenu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Organizatiom2019
 {
-    partial class ChangePass
+    partial class TechnicianMenu
     {
         /// <summary>
         /// Required designer variable.
@@ -30,20 +30,20 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -69,37 +69,8 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(438, 841);
+            this.panel1.Size = new System.Drawing.Size(437, 841);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(438, 315);
-            this.panel2.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = global::Organizatiom2019.Properties.Resources.Снимок;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(438, 315);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.button4);
-            this.panel3.Controls.Add(this.button1);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 315);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(438, 264);
-            this.panel3.TabIndex = 1;
             // 
             // panel4
             // 
@@ -108,30 +79,8 @@
             this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel4.Location = new System.Drawing.Point(0, 635);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(438, 206);
+            this.panel4.Size = new System.Drawing.Size(437, 206);
             this.panel4.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(107, 29);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(198, 93);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Назначенные заявки";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(94, 77);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(226, 54);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Мой проофиль";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
@@ -144,6 +93,27 @@
             this.button3.Text = "Выход";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(94, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Мой профиль";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 315);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(437, 264);
+            this.panel3.TabIndex = 1;
+            // 
             // button4
             // 
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
@@ -155,7 +125,37 @@
             this.button4.Text = "Удалить заявку";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // ChangePass
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(107, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 93);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Назначенные заявки";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(437, 315);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::Organizatiom2019.Properties.Resources.Снимок;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(437, 315);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TechnicianMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 38F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -163,14 +163,14 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
-            this.Name = "ChangePass";
+            this.Name = "TechnicianMenu";
             this.Text = "ChangePass";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel3.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
--- Organizatiom2019/Organizatiom2019/TechnicianMenu.cs
+++ Organizatiom2019/Organizatiom2019/TechnicianMenu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Organizatiom2019
 {
-    public partial class ChangePass : Form
+    public partial class TechnicianMenu : Form
     {
-        public ChangePass()
+        public TechnicianMenu()
         {
             InitializeComponent();
         }
